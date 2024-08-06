00ab853095296179edc05a79dbf486fb61f65d68
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-06 17:53:04 +0300

    documentation

--- utils/utilities.py
+++ utils/utilities.py
@@ -65,16 +65,6 @@ class Utilities:
                                   Each dictionary contains two keys:
                                   - 'label': The display name of the graph type.
                                   - 'value': The internal value used for identifying the graph type.
-
-        Example:
-            >>> App.get_supported_graphs()
-            [
-                {'label': 'Action Sequence by User', 'value': 'Action Sequence by User'},
-                {'label': 'Work Patterns Over Time', 'value': 'Work Patterns Over Time'},
-                {'label': 'Project Time Distribution', 'value': 'Project Time Distribution'},
-                {'label': 'Repeated Actions By User', 'value': 'Repeated Actions By User'},
-                {'label': 'Advanced vs. Basic Actions', 'value': 'Advanced vs. Basic Actions'},
-            ]
         """
         return [
             {'label': 'Action Sequence by User', 'value': 'Action Sequence by User'},
