0be493afc97d3d8b306eb4d023dce25a4cde0595
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-06 14:50:44 +0300

    Documentation

--- database/db_handler.py
+++ database/db_handler.py
@@ -1,5 +1,4 @@
 # Database Handler
-import json
 import logging
 from typing import Any
 
@@ -9,11 +8,28 @@ from config.constants import DatabaseCollections
 
 
 class DatabaseHandler:
+    """
+    A class to handle database operations with Firebase.
+
+    Attributes:
+        db (firebase.FirebaseApplication): The Firebase application instance.
+        logger (logging.Logger): Logger instance for logging information and errors.
+    """
     def __init__(self):
+        """Initialize the DatabaseHandler with no database connection and logger."""
         self.db = None
         self.logger = None
 
     def connect_to_firebase(self, db_url: str):
+        """
+        Connect to Firebase using the provided database URL.
+
+        Args:
+            db_url (str): The URL of the Firebase database.
+
+        Raises:
+            Exception: If connection to Firebase fails.
+        """
         try:
             self.db = firebase.FirebaseApplication(db_url, None)
             self.logger.info("Connected to Firebase successfully.")
@@ -22,9 +38,27 @@ class DatabaseHandler:
             raise Exception(f"Failed to connect to Firebase: {e}")
 
     def set_logger(self, logger: logging.Logger):
+        """
+        Set the logger for the DatabaseHandler.
+
+        Args:
+            logger (logging.Logger): Logger instance to be used for logging.
+        """
         self.logger = logger
 
     def read_from_database(self, collection_name: str) -> Any | None:
+        """
+        Read data from a specified collection in the database.
+
+        Args:
+            collection_name (str): The name of the collection to read from.
+
+        Returns:
+            Any | None: The data read from the database, or None if no data is found.
+
+        Raises:
+            Exception: If reading from the database fails.
+        """
         try:
             data = self.db.get(collection_name, None)
             if data is None:
@@ -36,6 +70,16 @@ class DatabaseHandler:
             raise e
 
     def write_to_database(self, collection_name: str, data: dict):
+        """
+        Write data to a specified collection in the database.
+
+        Args:
+            collection_name (str): The name of the collection to write to.
+            data (dict): The data to write to the database.
+
+        Raises:
+            Exception: If writing to the database fails.
+        """
         try:
             # Clear the collection if it's the default collection
             # This is to prevent the database from storing duplicate defaults
