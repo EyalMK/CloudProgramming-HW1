20fefc8a5a607ba881ada8c4a7aef3af9da57503
Eyal Maklada <wezkc21@gmail.com>
2024-08-07 20:45:30 +0300

    1. Ngrok back to production and testing environments

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from config.constants import RuntimeEnvironments, DB_CONN_URL, runtime_environme
 
 def setup_ngrok_auth():
     """
-    Configures Ngrok authentication if the runtime environment is DEV or TEST.
+    Configures Ngrok authentication if the runtime environment is PROD or TEST.
 
     This function sets the Ngrok authentication token based on the environment variable
     if the application is running in a development or testing environment.
     """
-    if runtime_environment in [RuntimeEnvironments.DEV.value, RuntimeEnvironments.TEST.value]:
+    if runtime_environment in [RuntimeEnvironments.PROD.value, RuntimeEnvironments.TEST.value]:
         conf.get_default().auth_token = os.environ.get("NGROK_TOKEN")
 
 
